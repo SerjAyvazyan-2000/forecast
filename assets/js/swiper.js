ad2c6584c9e2213fa7fe1ffd2a5d3135c520dfff
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-01-22 10:05:12 +0400

    new

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -26,6 +26,21 @@ let reviewsSwiper = new Swiper(".review-swiper", {
 
 });
 
+document.querySelector(".review-swiper").addEventListener("click", (event) => {
+    let swiperWrapper = document.querySelector(".review-swiper .swiper-wrapper");
+    let activeSlide = document.querySelector(".review-swiper .swiper-slide-active");
+
+    let wrapperRect = swiperWrapper.getBoundingClientRect();
+    let clickX = event.clientX;
+    let middleX = wrapperRect.left + wrapperRect.width / 2;
+
+    if (clickX > middleX) {
+        reviewsSwiper.slideNext();
+    } else {
+        reviewsSwiper.slidePrev();
+    }
+});
+
 let aboutSwiper = new Swiper(".about-swiper", {
     spaceBetween: 20,
     slidesPerView: 1,
